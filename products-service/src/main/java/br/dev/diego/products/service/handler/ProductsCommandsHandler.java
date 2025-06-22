805cc9bc9f1b5c346a67ffe1e03f65c813247831
Diego Sousa <me@diego.dev.br>
2025-06-22 00:31:11 -0300

    Reserve Product in Stock - business logic

--- products-service/src/main/java/br/dev/diego/products/service/handler/ProductsCommandsHandler.java
+++ products-service/src/main/java/br/dev/diego/products/service/handler/ProductsCommandsHandler.java
@@ -1,6 +1,10 @@
 package br.dev.diego.products.service.handler;
 
+import br.dev.diego.core.dto.Product;
 import br.dev.diego.core.dto.commands.ReserveProductCommand;
+import br.dev.diego.products.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,11 +14,24 @@ import org.springframework.stereotype.Component;
 @KafkaListener(topics = {"${products.commands.topic.name}"})
 public class ProductsCommandsHandler {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    private final ProductService productService;
+
+    public ProductsCommandsHandler(ProductService productService) {
+        this.productService = productService;
+    }
+
     @KafkaHandler
     public void handleCommand(@Payload ReserveProductCommand command) {
-        // Handle the command here
-        // This is a placeholder for actual command handling logic
-        System.out.println("Received command: " + command);
+
+        try {
+            Product desiredProduct = new Product(command.productId(), command.productQuantity());
+            Product reservedProduct = productService.reserve(desiredProduct, command.orderId());
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage(), e);
+        }
+
     }
 
 }
